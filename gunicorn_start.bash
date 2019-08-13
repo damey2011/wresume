56d4129578a0ac56b8ab6e5372150410e31f86a2
Damilola Adeyemi <neefemee@gmail.com>
2019-08-13 11:25:02 +0100

    update requirements and add gunicorn_start

---
+++ gunicorn_start.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+NAME="wresume"                                   # Name of the application
+DJANGODIR=/home/wresu/wresume               # Django project directory
+SOCKFILE=/home/wresu/env/run/gunicorn.sock  # we will communicte using this unix socket
+USER=wresu                                         # the user to run as
+GROUP=wresu                                        # the group to run as
+NUM_WORKERS=3                                       # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=wresume.settings      # which settings file should Django use
+DJANGO_WSGI_MODULE=wresume.wsgi              # WSGI module name
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+
+cd $DJANGODIR
+source /home/wresu/env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
