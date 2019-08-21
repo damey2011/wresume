5f5e9fd77e90134487cb00bcebc998e3371dc0a8
Damilola Adeyemi <neefemee@gmail.com>
2019-08-21 17:42:50 +0100

    fix static files issues

--- wresume/settings.py
+++ wresume/settings.py
@@ -189,6 +189,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
