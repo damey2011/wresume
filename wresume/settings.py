97e766ebffaff054a07c4821fafe59e187771613
Damilola Adeyemi <neefemee@gmail.com>
2019-08-21 17:47:10 +0100

    fix static settings

--- wresume/settings.py
+++ wresume/settings.py
@@ -186,10 +186,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_files'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
